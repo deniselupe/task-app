2120aa310a3e900ebc36d3bf7b43e4a05ddfaab2
dgrodriguez24 <dgrodriguez24@hotmail.com>
2022-09-07 21:43:14 -0400

    feat: moving form element to App component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import Overview from './components/Overview';
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            task: '',
+            taskArray: []
+        };
+    }
+
     render() {
+        const { task, taskArray } = this.state;
+
         return (
-            <div>
-                <Overview />
-            </div>
+          <div>
+            <form>
+              <label htmlFor='taskInput'>Enter Task:</label>
+              <input type='text' id='taskInput' />
+              <button>Add Task</button>
+            </form>
+          </div>
         );
     }
 }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,24 +1,9 @@
 import React, { Component } from 'react';
 
 class Overview extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            task: '',
-            taskArray: []
-        };
-    }
-
-    handleTask = (event) => {
-        this.setState({task: event.target.value});
-    };
-
     render() {
         return (
-            <div>
-                <input value={this.state.task} onChange={this.handleTask} />
-            </div>
+            <div></div>
         );
     }
 }
