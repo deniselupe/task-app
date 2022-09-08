4fce2ff380113a1a31dd6538fd6f8fc55e28266e
dgrodriguez24 <dgrodriguez24@hotmail.com>
2022-09-08 15:40:43 -0400

    feat: add event handlers to input and form

--- src/App.js
+++ src/App.js
@@ -2,28 +2,46 @@ import React, { Component } from 'react';
 import Overview from './components/Overview';
 
 class App extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            task: '',
-            taskArray: []
-        };
-    }
+    this.state = {
+      task: {text: ''},
+      taskArray: []
+    };
+  }
 
-    render() {
-        const { task, taskArray } = this.state;
+  handleChange = (event) => {
+    this.setState({
+      task: {
+        text: event.target.value
+      }
+    });
+  };
 
-        return (
-          <div>
-            <form>
-              <label htmlFor='taskInput'>Enter Task:</label>
-              <input type='text' id='taskInput' />
-              <button>Add Task</button>
-            </form>
-          </div>
-        );
-    }
+  onSubmitTask = (event) => {
+    event.preventDefault();
+    this.setState({
+      taskArray: [...this.state.taskArray, this.state.task],
+      task: {
+        text: ''
+      }
+    });
+  };
+
+  render() {
+    const { task, taskArray } = this.state;
+
+    return (
+      <div>
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor='taskInput'>Enter Task:</label>
+          <input type='text' id='taskInput' value={task.text} onChange={this.handleChange} />
+          <button type='submit'>Add Task</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
